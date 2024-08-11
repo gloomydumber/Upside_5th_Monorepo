afa5ec4c27ef3c1f3698eac25f8ea7ecf4be50e3
gloomydumber <chromacrow@naver.com>
2024-08-11 23:10:22 +0900

    `refactor` : order of function for readability

--- Upside_Practice_Lottery/src/Lottery.sol
+++ Upside_Practice_Lottery/src/Lottery.sol
@@ -25,6 +25,42 @@ contract Lottery {
         isBet[msg.sender] = true;
     }
 
+    function generateWinningNumber() internal view returns (uint16) {
+        return uint16(uint256( 
+            keccak256(
+                abi.encodePacked(
+                    block.timestamp,
+                    block.prevrandao,
+                    msg.sender,
+                    block.number
+                )
+            )
+        ) % 65536);
+    }
+
+    function draw() public {
+        require(block.timestamp >= (phaseStartTime + 24 hours), "Not Drawable: The Selling Phase is still ongoing");
+        winningNumber = generateWinningNumber();
+
+        uint256 winnerCount;
+        uint256 length = participants.length;
+        address[] memory winners = new address[](length);
+
+        for (uint256 i = 0; i < length; ++i) {
+            if (bet[participants[i]] == winningNumber && isBet[participants[i]] == true) {
+                winners[winnerCount] = participants[i];
+                ++winnerCount;
+            }
+        }
+
+        if (winnerCount > 0) {
+            uint256 prizePerWinner = address(this).balance / winnerCount;
+            for (uint256 i = 0; i < winnerCount; ++i) {
+                claimable[winners[i]] += prizePerWinner;
+            }
+        }
+    }
+
     function claim() public {
         require(block.timestamp >= (phaseStartTime + 24 hours), "Not Claimable: The Selling Phase is still ongoing");
         if (claimable[msg.sender] != 0) {
@@ -54,41 +90,5 @@ contract Lottery {
 
             phaseStartTime = block.timestamp;
         }
-    }
-
-    function draw() public {
-        require(block.timestamp >= (phaseStartTime + 24 hours), "Not Drawable: The Selling Phase is still ongoing");
-        winningNumber = generateWinningNumber();
-
-        uint256 winnerCount;
-        uint256 length = participants.length;
-        address[] memory winners = new address[](length);
-
-        for (uint256 i = 0; i < length; ++i) {
-            if (bet[participants[i]] == winningNumber && isBet[participants[i]] == true) {
-                winners[winnerCount] = participants[i];
-                ++winnerCount;
-            }
-        }
-
-        if (winnerCount > 0) {
-            uint256 prizePerWinner = address(this).balance / winnerCount;
-            for (uint256 i = 0; i < winnerCount; ++i) {
-                claimable[winners[i]] += prizePerWinner;
-            }
-        }
-    }
-
-    function generateWinningNumber() internal view returns (uint16) {
-        return uint16(uint256( 
-            keccak256(
-                abi.encodePacked(
-                    block.timestamp,
-                    block.prevrandao,
-                    msg.sender,
-                    block.number
-                )
-            )
-        ) % 65536);
     }
 }
